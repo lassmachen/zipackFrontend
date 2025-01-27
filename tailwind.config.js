26495d7fca5c448faa80c7dd9e2a33e3d0439f0b
EinfachRobbe <hi@einfachrobbe.de>
2025-01-27 18:59:19 +0100

    fixed fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ["./**/*.{razor,html,cshtml}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sulphur: ['"Sulphur Point"', 'sans-serif'],
-        racing: ['"Racing Sans One"', 'sans-serif'],
-        staatliches: ['"Staatliches"', 'sans-serif'],
-      },
-    },
   },
   plugins: [],
 }
