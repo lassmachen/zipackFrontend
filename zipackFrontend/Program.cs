f115abe3af49ebf38523b4bff44eb89a3f4a13b1
Suppenkasper787 <suppenkasper787@gmail.com>
2025-01-30 14:28:13 +0100

    Donation Page and Language switcher

--- zipackFrontend/Program.cs
+++ zipackFrontend/Program.cs
@@ -3,6 +3,8 @@ using zipackFrontend.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
